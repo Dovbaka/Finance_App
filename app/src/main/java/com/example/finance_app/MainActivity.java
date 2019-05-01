34e89fe08d445d162c086393decfcaaa2ccbbbc0
Dovbaka <37798027+Dovbaka@users.noreply.github.com>
2019-05-01 10:04:49 +0300

    Added primitive main page design

--- app/src/main/java/com/example/finance_app/MainActivity.java
+++ app/src/main/java/com/example/finance_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.finance_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -99,6 +100,11 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void onClick(View v){
+        Intent intent_Add = new Intent(this, Add_page.class);
+        startActivityForResult(intent_Add, 1);
     }
 
 
